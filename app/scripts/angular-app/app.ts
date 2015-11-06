0a28873756fb94279bb636744c08c0c869228f1e
Igor Minin <igogrek@gmail.com>
2015-11-06 21:06:09 +0300

    TSD fixes

--- app/scripts/angular-app/app.ts
+++ app/scripts/angular-app/app.ts
@@ -1,7 +1,6 @@
-/// <reference path="../../../typings/node.d.ts" />
-/// <reference path="../../../typings/lodash.d.ts" />
-/// <reference path="../../../typings/angular.d.ts" />
-/// <reference path="../../../typings/angular-ui-router.d.ts" />
+/// <reference path="../../../typings/lodash/lodash.d.ts" />
+/// <reference path="../../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../../typings/angular-ui-router/angular-ui-router.d.ts" />
 'use strict';
 
 import * as angular from 'angular';
